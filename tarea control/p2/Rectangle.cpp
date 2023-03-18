da36db26cd325f4dc20f4e07c611637319b382e2
Sebastián Sánchez <109748087+S6bastian@users.noreply.github.com>
2023-03-17 23:17:49 -0500

    Delete tarea control directory

--- tarea control/p2/Rectangle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Polygon.cpp"
-class Rectangle: public Polygon {
-    public:
-        Rectangle(int a, int b): Polygon(a,b) {}
-        int area () {return width * height;}
-        void printwh(){
-        cout << "Width: " <<width << " Height: " <<height << endl;}
-};
--- tarea control/p2/Triangle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Polygon.cpp"
-class Triangle: public Polygon {
-    public:
-        Triangle(int a, int b): Polygon(a,b) {}
-        int area () {return width * height / 2;}
-        void printwh(){
-        cout << "Width: " <<width << " Height: " <<height << endl;}
-};
